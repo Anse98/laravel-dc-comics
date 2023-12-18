a99384004703b8cde8fba136ddc1074a33ce190a
Anse98 <lorenzo.anselmi98.com>
2023-12-18 14:55:06 +0100

    ADD-Milestone 1

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
+
+class ComicController extends Controller
+{
+    
+}
