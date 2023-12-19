d6db87d164b1144321a9d0a34ef0e9b0c99814bc
Anse98 <lorenzo.anselmi98.com>
2023-12-19 19:15:45 +0100

    ADD-Menu per la conferma eliminazione e colonna 'price' messa in decimal

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -27,23 +27,33 @@
                         <td>
                             <img src="{{ $comic->thumb }}" width="80" >
                         </td>
-                        <td>{{ $comic->price }}</td>
+                        <td>${{ $comic->price }}</td>
                         <td>{{ $comic->series }}</td>
                         <td>{{ $comic->sale_date }}</td>
                         <td>{{ $comic->type }}</td>
                         <td class="">
                           <div class="d-flex gap-3">
 
-                            {{-- Pulsante modifica --}}
-                            <span><a href="{{route('comics.edit', $comic->id)}}" class="btn btn-primary">Modifica</a></span>
+                              {{-- Pulsante modifica --}}
+                              <span><a href="{{route('comics.edit', $comic->id)}}" class="btn btn-primary">Modifica</a></span>
 
-                            {{-- Pulsante elimina --}}
-                          <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <input type="submit" value="Elimina" class="btn btn-danger">
-                          </form>
+                              {{-- Modale --}}
+                              <div class="index" id="modal-delete-{{ $comic->id }}">
+                                <div class="delete-notification py-3 px-4">
+                                    <p class="mb-5 border-bottom"><b>Sei sicuro di voler eliminare questo elemento?</b></p>
+                                    <div class="d-flex justify-content-around">
+                                      <form action="{{route('comics.destroy', $comic)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        {{-- Pulsante elimina --}}
+                                        <input type="submit" value="Elimina" class="btn btn-danger">
+                                      </form>
+                                      <span class="btn btn-primary" onclick="hideModal()">Annulla</span>
+                                    </div>
+                                </div>
+                              </div>
+                            
+                            <span class="btn btn-danger" onclick="deleteNotification('{{ $comic->id }}')">Elimina</span>
                           </div>
                         </td>
                       </tr>
@@ -61,5 +71,24 @@
                 <span class="btn btn-primary"><a href="{{ route('comics.create') }}" class="text-decoration-none text-light">Aggiungi Fumetto</a></span>
               </div>
         </div>
-    </section>
+      </section>
+      
+      
+
+    <script>
+
+function deleteNotification(comicId) {
+    const deleteMenu = document.getElementById('modal-delete-' + comicId);
+    deleteMenu.classList.add("d-block");
+}
+
+function hideModal(comicId) {
+    const deleteMenu = document.getElementById('modal-delete-' + comicId);
+    deleteMenu.classList.remove('d-block');
+}
+
+      
+    </script>
+
 @endsection
+
